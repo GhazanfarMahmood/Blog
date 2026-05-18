dd7afbae0fcdb26422594baed7cc6d0dcbf6033f
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2026-05-18 18:02:52 +0500

    setting blog model

--- server/src/models/Blog.ts
+++ server/src/models/Blog.ts
@@ -3,6 +3,7 @@ import mongoose  from "mongoose";
 const blogSchema = new mongoose.Schema({
     title : {type : String, required : true},
     slug : {type: String, required: true},
+    excerpt : {type: String, required: true},
     content: {type: String, required: true},
     thumbnail : {type: String, required: true},
     category : {type: String, required: true},
