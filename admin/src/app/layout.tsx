2f03433841013d9562244860f10d791d7924d9fd
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2026-06-27 08:28:57 +0500

    setting up folder structure of admin panel

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// GOOGLE FONTS
+import { DM_Sans } from "next/font/google";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// GLOBALS CSS
+import "@/styles/globals.css";
+
+const dmSans = DM_Sans({
+  variable : "--font-dm-sans",
+  display : "swap",
+  weight : ["400", "800"],
+  subsets : ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${dmSans.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
