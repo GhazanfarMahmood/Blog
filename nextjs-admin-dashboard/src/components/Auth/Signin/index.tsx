eb30fb0d04e9b814c660539865138b31526409b9
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2026-07-10 08:04:12 +0500

    working on auth screen

--- nextjs-admin-dashboard/src/components/Auth/Signin/index.tsx
+++ nextjs-admin-dashboard/src/components/Auth/Signin/index.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import { Suspense } from "react";
-import GoogleSigninButton from "../GoogleSigninButton";
 import SigninWithPassword from "../SigninWithPassword";
 
 export default function Signin() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <GoogleSigninButton text="Sign in" />
 
       <div className="my-6 flex items-center justify-center">
         <span className="block h-px w-full bg-stroke dark:bg-dark-3"></span>
